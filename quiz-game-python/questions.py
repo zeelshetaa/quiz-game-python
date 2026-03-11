e5a1fd08758d3d3a91e70b7ce3343990f8bdd70e
zeelshetaa <zeelsheta1958@gmail.com>
2026-03-11 09:29:42 +0530

    Add files via upload

---
+++ quiz-game-python/questions.py
@@ -0,0 +1,27 @@
+question_data = [
+    {
+        "question": "What is the output of: print(2 ** 3)?",
+        "options": ["6", "8", "9", "5"],
+        "answer": "8"
+    },
+    {
+        "question": "Which data type is immutable in Python?",
+        "options": ["List", "Dictionary", "Tuple", "Set"],
+        "answer": "Tuple"
+    },
+    {
+        "question": "Which keyword is used to create a function in Python?",
+        "options": ["func", "define", "def", "lambda"],
+        "answer": "def"
+    },
+    {
+        "question": "Which library is used for data analysis?",
+        "options": ["NumPy", "Pandas", "TensorFlow", "Matplotlib"],
+        "answer": "Pandas"
+    },
+    {
+        "question": "Which operator is used for floor division?",
+        "options": ["/", "//", "%", "**"],
+        "answer": "//"
+    }
+]
